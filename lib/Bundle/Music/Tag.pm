fddd8bc4bd6089f46b8754d594cf3dddbff11326
Edward J. Allen III <ealleniii@cpan.org>
2008-02-12 05:16:42 +0000

    Import of EALLENIII/Bundle-Music-Tag-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Music-Tag
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         EALLENIII/Bundle-Music-Tag-0.01.tar.gz
    gitpan-cpan-author:       EALLENIII
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Music/Tag.pm
@@ -0,0 +1,61 @@
+package Bundle::Music::Tag;
+$VERSION = '0.01';
+1;
+__END__
+
+
+=head1 NAME
+
+Bundle::Music::Tag - Music::Tag bundle for quick installation
+
+=head1 SYNOPSIS
+
+ C<perl -MCPAN -e 'install Bundle::Music::Tag'>
+
+=head1 CONTENTS
+
+
+Term::ANSIColor
+
+Text::LevenshteinXS
+
+Text::Unaccent
+
+Time::Local
+
+Music::Tag
+
+Music::Tag::Amazon
+
+Music::Tag::FLAC
+
+Music::Tag::File
+
+Music::Tag::Lyrics
+
+Music::Tag::M4A
+
+Music::Tag::MP3
+
+Music::Tag::OGG
+
+
+=head1 DESCRIPTION
+
+This is a bundle for installing Music::Tag.  Use this to install everything previously
+included in Music::Tag.
+
+=head1 AUTHOR
+
+Edward Allen, ealleniii _at_ cpan _dot_ org
+
+=head1 LICENSE
+
+See tbe individual modules for full licence information.
+
+=head1 SEE ALSO
+
+L<Music::Tag>
+
+=cut
+
